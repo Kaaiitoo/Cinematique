dae049919238ea1ea969b1d2fecf240ea769ba20
Kaaiitoo <95180329+Kaaiitoo@users.noreply.github.com>
2023-12-30 21:11:41 +0100

    Add files via upload

---
+++ KCinematique/aeroport_depart.lua
@@ -0,0 +1,262 @@
+local sub_b0b5 = {
+    [0] = "MP_Plane_Passenger_1",
+    [1] = "MP_Plane_Passenger_2",
+    [2] = "MP_Plane_Passenger_3",
+    [3] = "MP_Plane_Passenger_4",
+    [4] = "MP_Plane_Passenger_5",
+    [5] = "MP_Plane_Passenger_6",
+    [6] = "MP_Plane_Passenger_7"
+}
+
+function sub_b747(ped, a_1)
+    if a_1 == 0 then
+        SetPedComponentVariation(ped, 0, 21, 0, 0)
+        SetPedComponentVariation(ped, 1, 0, 0, 0)
+        SetPedComponentVariation(ped, 2, 9, 0, 0)
+        SetPedComponentVariation(ped, 3, 1, 0, 0)
+        SetPedComponentVariation(ped, 4, 9, 0, 0)
+        SetPedComponentVariation(ped, 5, 0, 0, 0)
+        SetPedComponentVariation(ped, 6, 4, 8, 0)
+        SetPedComponentVariation(ped, 7, 0, 0, 0)
+        SetPedComponentVariation(ped, 8, 15, 0, 0)
+        SetPedComponentVariation(ped, 9, 0, 0, 0)
+        SetPedComponentVariation(ped, 10, 0, 0, 0)
+        SetPedComponentVariation(ped, 11, 10, 0, 0)
+        ClearPedProp(ped, 0)
+        ClearPedProp(ped, 1)
+        ClearPedProp(ped, 2)
+        ClearPedProp(ped, 3)
+        ClearPedProp(ped, 4)
+        ClearPedProp(ped, 5)
+        ClearPedProp(ped, 6)
+        ClearPedProp(ped, 7)
+        ClearPedProp(ped, 8)
+    elseif a_1 == 1 then
+        SetPedComponentVariation(ped, 0, 13, 0, 0)
+        SetPedComponentVariation(ped, 1, 0, 0, 0)
+        SetPedComponentVariation(ped, 2, 5, 4, 0)
+        SetPedComponentVariation(ped, 3, 1, 0, 0)
+        SetPedComponentVariation(ped, 4, 10, 0, 0)
+        SetPedComponentVariation(ped, 5, 0, 0, 0)
+        SetPedComponentVariation(ped, 6, 10, 0, 0)
+        SetPedComponentVariation(ped, 7, 11, 2, 0)
+        SetPedComponentVariation(ped, 8, 13, 6, 0)
+        SetPedComponentVariation(ped, 9, 0, 0, 0)
+        SetPedComponentVariation(ped, 10, 0, 0, 0)
+        SetPedComponentVariation(ped, 11, 10, 0, 0)
+        ClearPedProp(ped, 0)
+        ClearPedProp(ped, 1)
+        ClearPedProp(ped, 2)
+        ClearPedProp(ped, 3)
+        ClearPedProp(ped, 4)
+        ClearPedProp(ped, 5)
+        ClearPedProp(ped, 6)
+        ClearPedProp(ped, 7)
+        ClearPedProp(ped, 8)
+    elseif a_1 == 2 then
+        SetPedComponentVariation(ped, 0, 15, 0, 0)
+        SetPedComponentVariation(ped, 1, 0, 0, 0)
+        SetPedComponentVariation(ped, 2, 1, 4, 0)
+        SetPedComponentVariation(ped, 3, 1, 0, 0)
+        SetPedComponentVariation(ped, 4, 0, 1, 0)
+        SetPedComponentVariation(ped, 5, 0, 0, 0)
+        SetPedComponentVariation(ped, 6, 1, 7, 0)
+        SetPedComponentVariation(ped, 7, 0, 0, 0)
+        SetPedComponentVariation(ped, 8, 2, 9, 0)
+        SetPedComponentVariation(ped, 9, 0, 0, 0)
+        SetPedComponentVariation(ped, 10, 0, 0, 0)
+        SetPedComponentVariation(ped, 11, 6, 0, 0)
+        ClearPedProp(ped, 0)
+        ClearPedProp(ped, 1)
+        ClearPedProp(ped, 2)
+        ClearPedProp(ped, 3)
+        ClearPedProp(ped, 4)
+        ClearPedProp(ped, 5)
+        ClearPedProp(ped, 6)
+        ClearPedProp(ped, 7)
+        ClearPedProp(ped, 8)
+    elseif a_1 == 3 then
+        SetPedComponentVariation(ped, 0, 14, 0, 0)
+        SetPedComponentVariation(ped, 1, 0, 0, 0)
+        SetPedComponentVariation(ped, 2, 5, 3, 0)
+        SetPedComponentVariation(ped, 3, 3, 0, 0)
+        SetPedComponentVariation(ped, 4, 1, 6, 0)
+        SetPedComponentVariation(ped, 5, 0, 0, 0)
+        SetPedComponentVariation(ped, 6, 11, 5, 0)
+        SetPedComponentVariation(ped, 7, 0, 0, 0)
+        SetPedComponentVariation(ped, 8, 2, 0, 0)
+        SetPedComponentVariation(ped, 9, 0, 0, 0)
+        SetPedComponentVariation(ped, 10, 0, 0, 0)
+        SetPedComponentVariation(ped, 11, 3, 12, 0)
+        ClearPedProp(ped, 0)
+        ClearPedProp(ped, 1)
+        ClearPedProp(ped, 2)
+        ClearPedProp(ped, 3)
+        ClearPedProp(ped, 4)
+        ClearPedProp(ped, 5)
+        ClearPedProp(ped, 6)
+        ClearPedProp(ped, 7)
+        ClearPedProp(ped, 8)
+    elseif a_1 == 4 then
+        SetPedComponentVariation(ped, 0, 18, 0, 0)
+        SetPedComponentVariation(ped, 1, 0, 0, 0)
+        SetPedComponentVariation(ped, 2, 15, 3, 0)
+        SetPedComponentVariation(ped, 3, 15, 0, 0)
+        SetPedComponentVariation(ped, 4, 2, 5, 0)
+        SetPedComponentVariation(ped, 5, 0, 0, 0)
+        SetPedComponentVariation(ped, 6, 4, 6, 0)
+        SetPedComponentVariation(ped, 7, 4, 0, 0)
+        SetPedComponentVariation(ped, 8, 3, 0, 0)
+        SetPedComponentVariation(ped, 9, 0, 0, 0)
+        SetPedComponentVariation(ped, 10, 0, 0, 0)
+        SetPedComponentVariation(ped, 11, 4, 0, 0)
+        ClearPedProp(ped, 0)
+        ClearPedProp(ped, 1)
+        ClearPedProp(ped, 2)
+        ClearPedProp(ped, 3)
+        ClearPedProp(ped, 4)
+        ClearPedProp(ped, 5)
+        ClearPedProp(ped, 6)
+        ClearPedProp(ped, 7)
+        ClearPedProp(ped, 8)
+    elseif a_1 == 5 then
+        SetPedComponentVariation(ped, 0, 27, 0, 0)
+        SetPedComponentVariation(ped, 1, 0, 0, 0)
+        SetPedComponentVariation(ped, 2, 7, 3, 0)
+        SetPedComponentVariation(ped, 3, 11, 0, 0)
+        SetPedComponentVariation(ped, 4, 4, 8, 0)
+        SetPedComponentVariation(ped, 5, 0, 0, 0)
+        SetPedComponentVariation(ped, 6, 13, 14, 0)
+        SetPedComponentVariation(ped, 7, 5, 3, 0)
+        SetPedComponentVariation(ped, 8, 3, 0, 0)
+        SetPedComponentVariation(ped, 9, 0, 0, 0)
+        SetPedComponentVariation(ped, 10, 0, 0, 0)
+        SetPedComponentVariation(ped, 11, 2, 7, 0)
+        ClearPedProp(ped, 0)
+        ClearPedProp(ped, 1)
+        ClearPedProp(ped, 2)
+        ClearPedProp(ped, 3)
+        ClearPedProp(ped, 4)
+        ClearPedProp(ped, 5)
+        ClearPedProp(ped, 6)
+        ClearPedProp(ped, 7)
+        ClearPedProp(ped, 8)
+    elseif a_1 == 6 then
+        SetPedComponentVariation(ped, 0, 16, 0, 0)
+        SetPedComponentVariation(ped, 1, 0, 0, 0)
+        SetPedComponentVariation(ped, 2, 15, 1, 0)
+        SetPedComponentVariation(ped, 3, 3, 0, 0)
+        SetPedComponentVariation(ped, 4, 5, 6, 0)
+        SetPedComponentVariation(ped, 5, 0, 0, 0)
+        SetPedComponentVariation(ped, 6, 2, 8, 0)
+        SetPedComponentVariation(ped, 7, 0, 0, 0)
+        SetPedComponentVariation(ped, 8, 2, 0, 0)
+        SetPedComponentVariation(ped, 9, 0, 0, 0)
+        SetPedComponentVariation(ped, 10, 0, 0, 0)
+        SetPedComponentVariation(ped, 11, 3, 7, 0)
+        ClearPedProp(ped, 0)
+        ClearPedProp(ped, 1)
+        ClearPedProp(ped, 2)
+        ClearPedProp(ped, 3)
+        ClearPedProp(ped, 4)
+        ClearPedProp(ped, 5)
+        ClearPedProp(ped, 6)
+        ClearPedProp(ped, 7)
+        ClearPedProp(ped, 8)
+    end
+end
+
+RegisterNetEvent('introCinematic:start') -- ORDER CREATION
+AddEventHandler('introCinematic:start', function()
+	PrepareMusicEvent("FM_INTRO_START") --FM_INTRO_START
+	TriggerMusicEvent("FM_INTRO_START") --FM_INTRO_START
+    local plyrId = PlayerPedId() -- PLAYER ID
+    -----------------------------------------------
+	if IsMale(plyrId) then
+		RequestCutsceneWithPlaybackList("MP_INTRO_CONCAT", 31, 8)
+	else	
+		RequestCutsceneWithPlaybackList("MP_INTRO_CONCAT", 103, 8)
+	end
+    while not HasCutsceneLoaded() do Wait(10) end --- waiting for the cutscene to load
+	if IsMale(plyrId) then
+		RegisterEntityForCutscene(0, 'MP_Male_Character', 3, GetEntityModel(PlayerPedId()), 0)
+		RegisterEntityForCutscene(PlayerPedId(), 'MP_Male_Character', 0, 0, 0)
+		SetCutsceneEntityStreamingFlags('MP_Male_Character', 0, 1) 
+		local female = RegisterEntityForCutscene(0,"MP_Female_Character",3,0,64) 
+		NetworkSetEntityInvisibleToNetwork(female, true)
+	else
+		RegisterEntityForCutscene(0, 'MP_Female_Character', 3, GetEntityModel(PlayerPedId()), 0)
+		RegisterEntityForCutscene(PlayerPedId(), 'MP_Female_Character', 0, 0, 0)
+		SetCutsceneEntityStreamingFlags('MP_Female_Character', 0, 1) 
+		local male = RegisterEntityForCutscene(0,"MP_Male_Character",3,0,64) 
+		NetworkSetEntityInvisibleToNetwork(male, true)
+	end
+	local ped = {}
+	for v_3=0, 6, 1 do
+		if v_3 == 1 or v_3 == 2 or v_3 == 4 or v_3 == 6 then
+			ped[v_3] = CreatePed(26, `mp_f_freemode_01`, -1117.77783203125, -1557.6248779296875, 3.3819, 0.0, 0, 0)
+		else
+			ped[v_3] = CreatePed(26, `mp_m_freemode_01`, -1117.77783203125, -1557.6248779296875, 3.3819, 0.0, 0, 0)
+		end
+        if not IsEntityDead(ped[v_3]) then
+			sub_b747(ped[v_3], v_3)
+            FinalizeHeadBlend(ped[v_3])
+            RegisterEntityForCutscene(ped[v_3], sub_b0b5[v_3], 0, 0, 64)
+        end
+    end
+	
+	NewLoadSceneStartSphere(-1212.79, -1673.52, 7, 1000, 0) ----- avoid texture bugs
+    -----------------------------------------------
+    SetWeatherTypeNow("EXTRASUNNY") ---- SUN TIME
+    StartCutscene(4) --- START the custscene
+
+    Wait(38600) --- custscene time
+     StopCutsceneImmediately()
+	for v_3=0, 6, 1 do
+		DeleteEntity(ped[v_3])
+	end
+	PrepareMusicEvent("AC_STOP")
+	TriggerMusicEvent("AC_STOP")
+end) 
+
+
+function IsMale(ped)
+	if IsPedModel(ped, 'mp_m_freemode_01') then
+		return true
+	else
+		return false
+	end
+end
+
+local position = {
+    {x = -1234.88, y = -2986.49, z = -41.27}
+}
+
+Citizen.CreateThread(function()
+    while true do
+
+      local wait = 750
+
+        for k in pairs(position) do
+            local plyCoords = GetEntityCoords(GetPlayerPed(-1), false)
+            local dist = Vdist(plyCoords.x, plyCoords.y, plyCoords.z, position[k].x, position[k].y, position[k].z)
+
+            if dist <= 1.0 then
+                RageUI.Text({
+
+                    message = "Appuyez sur [~b~ENTRER~s~] pour partir à Los Santos",
+        
+                    time_display = 1
+        
+                })
+               wait = 0
+                if IsControlJustPressed(1,176) then
+                TriggerEvent('introCinematic:start')
+    end
+    end
+    Citizen.Wait(wait)
+    end
+end
+end)
+
+
